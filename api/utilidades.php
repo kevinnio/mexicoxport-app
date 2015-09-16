8f74ff368501973b083972f98685085f9102daa7
Kevin Perez <kevindperezm@gmail.com>
2015-09-16 11:26:59 -0500

    añadir utf8_encode_all() a utilidades de servicio web

--- api/utilidades.php
+++ api/utilidades.php
@@ -12,3 +12,20 @@ require_once '../mxport.php';
 function sanitizar($dato) {
   return mysqli_escape_string(getMyConection(), $dato);
 }
+
+/**
+ * Encodea a UTF-8 todas las cadenas dentro de un array.
+ *
+ * @param $data
+ *
+ * @return array|string
+ */
+function utf8_encode_all($data) {
+  if (is_string($data)) return utf8_encode($data);
+  if ( ! is_array($data)) return $data;
+
+  $result = array();
+  foreach($data as $i=>$d) $result[$i] = utf8_encode_all($d);
+
+  return $result;
+}
