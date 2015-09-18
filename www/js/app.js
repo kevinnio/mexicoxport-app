a9bead8b8f475c1932e8db8981d672780bc6ba14
Kevin Perez <kevindperezm@gmail.com>
2015-09-18 10:12:24 -0500

    redefinir las rutas del app
    Esto elimina rutas no necesarias y renombra las que sí se usarán en el app.

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,7 @@
-// Ionic Starter App
-
-angular.module('underscore', [])
-.factory('_', function() {
-  return window._; // assumes underscore has already been loaded on the page
+angular.module('underscore', []).factory('_', function() {
+  return window._;
 });
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('mexicoxport', [
   'ionic',
   'angularMoment',
@@ -30,11 +24,10 @@ angular.module('mexicoxport', [
 .run(function($ionicPlatform, PushNotificationsService, $rootScope, $ionicConfig, $timeout) {
 
   $ionicPlatform.on("deviceready", function(){
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
+
     if(window.StatusBar) {
       StatusBar.styleDefault();
     }
@@ -42,25 +35,9 @@ angular.module('mexicoxport', [
     PushNotificationsService.register();
   });
 
-  // This fixes transitions for transparent background views
-  $rootScope.$on("$stateChangeSuccess", function(event, toState, toParams, fromState, fromParams){
-    if(toState.name.indexOf('app.feeds-categories') > -1)
-    {
-      // Restore platform default transition. We are just hardcoding android transitions to auth views.
-      $ionicConfig.views.transition('platform');
-      // If it's ios, then enable swipe back again
-      if(ionic.Platform.isIOS())
-      {
-        $ionicConfig.views.swipeBackEnabled(true);
-      }
-    	console.log("enabling swipe back and restoring transition to platform default", $ionicConfig.views.transition());
-    }
-  });
-
   $ionicPlatform.on("resume", function(){
     PushNotificationsService.register();
   });
-
 })
 
 
@@ -73,31 +50,38 @@ angular.module('mexicoxport', [
     templateUrl: "views/app/side-menu.html",
     controller: 'AppCtrl'
   })
-    
 
-  .state('app.feed-entries', {
-    url: "/feed-entries/:categoryId/:sourceId",
+  .state('app.noticias', {
+    url: "/noticias",
     views: {
       'menuContent': {
-        templateUrl: "views/app/feeds/feed-entries.html",
-        controller: 'FeedEntriesCtrl'
+        templateUrl: "views/app/noticias/index.html",
+        controller: 'NoticiasCtrl'
       }
     }
   })
 
-  //OTHERS
-  .state('app.settings', {
-    url: "/settings",
+  .state('app.categorias.noticias', {
+    url: "/categoria/:categoriaId",
     views: {
       'menuContent': {
-        templateUrl: "views/app/settings.html",
-        controller: 'SettingsCtrl'
+        templateUrl: "views/app/noticias/index.html",
+        controller: 'CategoriasCtrl'
+      }
+    }
+  })
+
+  .state('app.ajustes', {
+    url: "/ajustes",
+    views: {
+      'menuContent': {
+        templateUrl: "views/app/ajustes.html",
+        controller: 'AjustesCtrl'
       }
     }
   })
 
 ;
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/feeds-categories');
+  $urlRouterProvider.otherwise('/app/noticias');
 });
