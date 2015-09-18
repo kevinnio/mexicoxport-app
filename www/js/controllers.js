7f3f033b8f7dc2120bf76f0017bf83b37f957633
Kevin Perez <kevindperezm@gmail.com>
2015-09-18 10:23:39 -0500

    cargar primeras 20 noticias al abrir el app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,56 +4,36 @@ angular.module('mexicoxport.controllers', [])
 
 })
 
-.controller('NoticiasCtrl', function($scope, $stateParams, $http, FeedList, $q, $ionicLoading, BookMarkService) {
-	$scope.feed = [];
+.controller('NoticiasCtrl', function($scope, $http, $ionicLoading) {
+  $scope.noticias = [];
 
-	var categoryId = $stateParams.categoryId,
-			sourceId = $stateParams.sourceId;
+  $scope.doRefresh = function() {
+    $ionicLoading.show({
+      template: 'Cargando noticias...'
+    });
 
-	$scope.doRefresh = function() {
+    $http.get('http://mexicoxport.com/api/noticias.php').success(function(response) {
+      $scope.noticias = response;
 
-		$http.get('noticias-categories.json').success(function(response) {
+      $ionicLoading.hide();
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
 
-			$ionicLoading.show({
-				template: 'Loading entries...'
-			});
-
-			var category = _.find(response, {id: categoryId }),
-					source = _.find(category.feed_sources, {id: sourceId });
-
-			$scope.sourceTitle = source.title;
-
-			FeedList.get(source.url)
-			.then(function (result) {
-				$scope.feed = result.feed;
-				$ionicLoading.hide();
-				$scope.$broadcast('scroll.refreshComplete');
-			}, function (reason) {
-				$ionicLoading.hide();
-				$scope.$broadcast('scroll.refreshComplete');
-			});
-		});
-	};
-
-	$scope.doRefresh();
-
-	$scope.bookmarkPost = function(post){
-		$ionicLoading.show({ template: 'Post Saved!', noBackdrop: true, duration: 1000 });
-		BookMarkService.bookmarkFeedPost(post);
-	};
+  $scope.doRefresh();
 })
 
 .controller('AjustesCtrl', function($scope, $ionicActionSheet, $state) {
-	$scope.airplaneMode = true;
-	$scope.wifi = false;
-	$scope.bluetooth = true;
-	$scope.personalHotspot = true;
+  $scope.airplaneMode = true;
+  $scope.wifi = false;
+  $scope.bluetooth = true;
+  $scope.personalHotspot = true;
 
-	$scope.checkOpt1 = true;
-	$scope.checkOpt2 = true;
-	$scope.checkOpt3 = false;
+  $scope.checkOpt1 = true;
+  $scope.checkOpt2 = true;
+  $scope.checkOpt3 = false;
 
-	$scope.radioChoice = 'B';
+  $scope.radioChoice = 'B';
 })
 
 ;
