d2db1dbd88aa55303a9ae51df19ca8325decdbd0
Kevin Perez <kevindperezm@gmail.com>
2015-09-25 13:30:46 -0500

    recuperar factory para NodePushServer
    Esta factory es requerida por el service PushNotificationsService. Había sido
    removida, pero este commit la recupera.

--- www/js/factories.js
+++ www/js/factories.js
@@ -1,32 +1,76 @@
-angular.module('mexicoxport.factories', [])
+var factories = angular.module('mexicoxport.factories', []);
 
-.factory('AlmacenNoticias', function() {
-    return {
-      noticias: [],
+factories.factory('NodePushServer', function ($http) {
+  // Configure push notifications server address
+  //                 - If you are running a local push notifications server you can test this by setting
+  var push_server_address = "http://192.168.1.102:8000";
 
-      buscar: function(id) {
-        for (var i = 0; i < this.noticias.length; i++) {
-          if (this.noticias[i].idNoticia == id) return this.noticias[i];
-        }
-      },
+  return {
+    // Stores the device token in a db using node-pushserver
+    // type:  Platform type (ios, android etc)
+    storeDeviceToken: function(type, regId) {
+      // Create a random userid to store with it
+      var user = {
+        user: 'user' + Math.floor((Math.random() * 10000000) + 1),
+        type: type,
+        token: regId
+      };
+      console.log("Post token for registered device with data " + JSON.stringify(user));
 
-      agregar: function(noticias) {
-        if (noticias.constructor === Array) {
-          for (var i = 0; i < noticias.length; this.noticias.push(noticias[i++]));
-        } else {
-          this.noticias.push(noticias);
-        }
-      },
+      $http.post(push_server_address+'/subscribe', JSON.stringify(user))
+      .success(function (data, status) {
+        console.log("Token stored, device is successfully subscribed to receive push notifications.");
+      })
+      .error(function (data, status) {
+        console.log("Error storing device token." + data + " " + status);
+      });
+    },
+    // CURRENTLY NOT USED!
+    // Removes the device token from the db via node-pushserver API unsubscribe (running locally in this
+    // If you registered the same device with different userids, *ALL* will be removed. (It's recommende
+    // time the app opens which this currently does. However in many cases you will always receive the s
+    // previously so multiple userids will be created with the same token unless you add code to check).
+    removeDeviceToken: function(token) {
+      var tkn = {"token": token};
+      $http.post(push_server_address+'/unsubscribe', JSON.stringify(tkn))
+      .success(function (data, status) {
+        console.log("Token removed, device is successfully unsubscribed and will not receive push notifications");
+      })
+      .error(function (data, status) {
+        console.log("Error removing device token." + data + " " + status);
+      });
+    }
+  };
+});
 
-      ultimaNoticia: function() {
-        return this.noticias[this.noticias.length - 1];
-      },
 
-      vaciar: function() {
-        delete this.noticias;
-        this.noticias = [];
+factories.factory('AlmacenNoticias', function() {
+  return {
+    noticias: [],
+
+    buscar: function(id) {
+      for (var i = 0; i < this.noticias.length; i++) {
+        if (this.noticias[i].idNoticia == id) return this.noticias[i];
       }
-    };
-})
 
-;
+      return null;
+    },
+
+    agregar: function(noticias) {
+      if (noticias.constructor === Array) {
+        for (var i = 0; i < noticias.length; this.noticias.push(noticias[i++]));
+      } else {
+        this.noticias.push(noticias);
+      }
+    },
+
+    ultimaNoticia: function() {
+      return this.noticias[this.noticias.length - 1];
+    },
+
+    vaciar: function() {
+      delete this.noticias;
+      this.noticias = [];
+    }
+  };
+});
