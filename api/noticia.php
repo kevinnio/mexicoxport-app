f1f520d08c54db55c1d35b9b858a661fec178665
Kevin Perez <kevindperezm@gmail.com>
2015-10-10 00:47:49 -0500

    renombrar campos de noticia al mostrar
    Esto modifica la API para que renombre los campos de la noticia antes de
    enviarla al app móvil. Esto evitará que el app móvil utilice los nombres de
    los campos reales en la base de datos y facilitará que el app funcione
    incluso cuando los nombres de los campos cambien.

--- api/noticia.php
+++ api/noticia.php
@@ -10,13 +10,25 @@
 require_once '../mxport.php';
 require_once __DIR__.'/utilidades.php';
 
+$campos_de_noticias = array(
+  'id'      => 'idNoticia',
+  'titulo'  => 'Titulo',
+  'resumen' => 'Resumen',
+  'cuerpo'  => 'Cuerpo',
+  'imagen'  => 'Imagen',
+  'fuente'  => 'Fuente',
+  'fecha'   => 'FechaNoticia',
+  'vistas'  => 'Views',
+);
+
 extract($_GET);
 
-$campos    = 'idNoticia, Titulo, Resumen, Cuerpo, Imagen, Fuente, Views';
+$campos    = implode(', ', array_values($campos_de_noticias));
 $resultado = query("SELECT $campos FROM noticias WHERE idNoticia = " . sanitizar($idNoticia));
 
 if ($resultado) {
-  $noticia = mysqli_fetch_assoc($resultado);
+  $noticia = renombrar_campos(array(mysqli_fetch_assoc($resultado)), $campos_de_noticias);
+  $noticia = $noticia[0];
   $views = $noticia["Views"] + 1;
   query("UPDATE noticias SET Views = $views WHERE idNoticia = ". sanitizar($idNoticia));
   enviarRespuesta($noticia);
