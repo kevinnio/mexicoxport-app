839102540a183962d182b20b7bac6eaf6e39321f
Kevin Perez <kevindperezm@gmail.com>
2015-11-07 11:16:38 -0600

    añadir constante para clave de API

--- www/js/config.js
+++ www/js/config.js
@@ -1,5 +1,6 @@
 angular.module('mexicoxport.config', [])
-.constant('WORDPRESS_API_URL', 'http://wordpress.startapplabs.com/blog/api/')
+
 .constant('GCM_SENDER_ID', '574597432927')
+.constant('GOOGLE_API_KEY', 'AIzaSyCsShE3VHWlZZIUIyzWN4VgaJ7olnH6Obo')
 
 ;
