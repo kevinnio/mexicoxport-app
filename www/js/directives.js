196603f8868b63cfe5119467701455328fc99a41
Kevin Perez <kevindperezm@gmail.com>
2015-11-10 10:29:34 -0600

    añadir directiva para abrir link externamente

--- www/js/directives.js
+++ www/js/directives.js
@@ -68,3 +68,22 @@ directives.directive('videoSmall', function() {
     templateUrl: 'views/common/video-small.html'
   };
 });
+
+directives.directive('browseTo', function ($ionicGesture) {
+  return {
+    restrict: 'A',
+    link: function ($scope, $element, $attrs) {
+      var handleTap = function (e) {
+        // todo: capture Google Analytics here
+        var inAppBrowser = window.open(encodeURI($attrs.browseTo), '_system');
+      };
+
+      var tapGesture = $ionicGesture.on('tap', handleTap, $element);
+
+      $scope.$on('$destroy', function () {
+        // Clean up - unbind drag gesture handler
+        $ionicGesture.off(tapGesture, 'tap', handleTap);
+      });
+    }
+  }
+});
