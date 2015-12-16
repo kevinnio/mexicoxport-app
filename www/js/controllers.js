ec057d3d8981eb30906921a0babfb37255b599d7
Kevin Perez <kevindperezm@gmail.com>
2015-12-15 22:39:27 -0600

    añadir botón provisional para compartir
    Este botón se usará para probar que la feature funciona
    correctamente. Luego se le dará estilo y se le colocará en una
    mejor posición en la pantalla.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,39 @@
 var controllers = angular.module('mexicoxport.controllers', []);
 
-controllers.controller('AppCtrl', function($scope, $location, AlmacenCategorias, DescargarCategoriasService) {
+controllers.controller('AppCtrl', function ($scope, $location, AlmacenCategorias, DescargarCategoriasService) {
   $scope.version = '1.0';
   $scope.categorias = AlmacenCategorias.todas();
   $scope.$location = $location;
 
   if ($scope.categorias.length <= 0) {
-    DescargarCategoriasService.categorias(function(categorias) {
+    DescargarCategoriasService.categorias(function (categorias) {
       for (var i = 0; i < categorias.length; AlmacenCategorias.agregar(categorias[i++]));
     });
   }
 });
 
-controllers.controller('NoticiasCtrl', function($scope, DescargarNoticiasService) {
+controllers.controller('NoticiasCtrl', function ($scope, DescargarNoticiasService) {
   $scope.noticias = [];
   $scope.infiniteScroll = true;
 
-  $scope.refrescar = function() {
+  $scope.refrescar = function () {
     $scope.noticias = [];
     $scope.cargar();
   };
 
-  $scope.cargar = function() {
-    DescargarNoticiasService.recientes($scope.noticias.length, null, function(noticias) {
+  $scope.cargar = function () {
+    DescargarNoticiasService.recientes($scope.noticias.length, null, function (noticias) {
       $scope.postCargar(noticias);
     });
   };
 
-  $scope.postCargar = function(noticias) {
+  $scope.postCargar = function (noticias) {
     $scope.noticias = $scope.noticias.concat(noticias);
     $scope.$broadcast('scroll.refreshComplete');
     $scope.$broadcast('scroll.infiniteScrollComplete');
   };
 
-  $scope.obtenerUltimaNoticia = function() {
+  $scope.obtenerUltimaNoticia = function () {
     var ultimaNoticia;
 
     if ($scope.noticias.length > 0) {
@@ -44,41 +44,55 @@ controllers.controller('NoticiasCtrl', function($scope, DescargarNoticiasService
 
     return ultimaNoticia;
   };
+
 });
 
-controllers.controller('NoticiaCtrl', function($scope, $stateParams, $ionicLoading, DescargarNoticiasService) {
+controllers.controller('NoticiaCtrl', function ($scope, $stateParams, $ionicLoading, $cordovaSocialSharing, DescargarNoticiasService) {
   $ionicLoading.show({
     hideOnStateChange: true
   });
 
-  DescargarNoticiasService.noticia($stateParams.id, function(noticia) {
+  DescargarNoticiasService.noticia($stateParams.id, function (noticia) {
     $scope.noticia = noticia;
     $ionicLoading.hide();
   });
+
+  $scope.compartirNoticia = function(noticia) {
+    $cordovaSocialSharing.share(noticia.titulo,
+                                noticia.resumen,
+                                null,
+                                "http://mexicoxport.com" + noticia.url)
+      .then(function(result) {
+        console.log('Exito al compartir');
+      }, function(err) {
+        console.log('Error al compartir');
+        console.log(err);
+      });
+  };
 });
 
-controllers.controller('CategoriaCtrl', function($controller, $scope, $stateParams, DescargarNoticiasService) {
-  $controller('NoticiasCtrl', { $scope: $scope });
+controllers.controller('CategoriaCtrl', function ($controller, $scope, $stateParams, DescargarNoticiasService) {
+  $controller('NoticiasCtrl', {$scope: $scope});
 
-  $scope.cargar = function() {
+  $scope.cargar = function () {
     var categoriaId = $stateParams.id;
-    DescargarNoticiasService.recientes($scope.noticias.length, categoriaId, function(noticias) {
+    DescargarNoticiasService.recientes($scope.noticias.length, categoriaId, function (noticias) {
       $scope.postCargar(noticias);
     });
   };
 });
 
-controllers.controller('TopCtrl', function($controller, $scope, $ionicLoading, DescargarNoticiasService) {
-  $controller('NoticiasCtrl', { $scope: $scope });
+controllers.controller('TopCtrl', function ($controller, $scope, $ionicLoading, DescargarNoticiasService) {
+  $controller('NoticiasCtrl', {$scope: $scope});
 
   $scope.infiniteScroll = false;
 
-  $scope.cargar = function() {
+  $scope.cargar = function () {
     $ionicLoading.show({
       hideOnStateChange: true
     });
 
-    DescargarNoticiasService.top(function(noticias) {
+    DescargarNoticiasService.top(function (noticias) {
       $scope.postCargar(noticias);
       $ionicLoading.hide();
     });
@@ -87,26 +101,26 @@ controllers.controller('TopCtrl', function($controller, $scope, $ionicLoading, D
   $scope.cargar();
 });
 
-controllers.controller('TvCtrl', function($scope, TvService) {
+controllers.controller('TvCtrl', function ($scope, TvService) {
   $scope.videos = [];
   $scope.service = TvService;
 
-  $scope.nextPage = function() {
-    $scope.service.nextPage(function(videos) {
+  $scope.nextPage = function () {
+    $scope.service.nextPage(function (videos) {
       $scope.videos = $scope.videos.concat(videos);
       $scope.$broadcast('scroll.infiniteScrollComplete');
       $scope.$broadcast('scroll.refreshComplete');
     });
   };
 
-  $scope.refresh = function() {
+  $scope.refresh = function () {
     $scope.videos = [];
     $scope.service.reset();
     $scope.nextPage();
   };
 
-  $scope.canLoad = function() {
+  $scope.canLoad = function () {
     return $scope.videos.length <= 0 ||
-           $scope.videos.length < $scope.service.getTotal();
+      $scope.videos.length < $scope.service.getTotal();
   };
 });
