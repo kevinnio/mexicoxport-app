c10991c2e2406b3fb152194af3807284efd6828d
Kevin Perez <kevindperezm@gmail.com>
2016-01-05 17:17:23 -0600

    añadir endpoint que devuelve contenido de iframe
    Una técnica para poner comentarios de Facebook en el app es usar un iframe que
    cargue la página desde donde se cargue el plugin necesario. Esto añade un
    endpoint a la API que devuelve lo que debe ir dentro del iframe.

---
+++ api/facebook-comments.php
@@ -0,0 +1,13 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/all.js#xfbml=1&appId=238105652928008";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-comments"
+     data-href="<?php echo htmlspecialchars($_GET['news-url']) ?>"
+     data-numposts="5">
+</div>
