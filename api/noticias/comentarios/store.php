4ecace16f269d43e1a30b75e3b9157c0d6d1a535
Kevin Perez <kevindperezm@gmail.com>
2016-02-18 00:56:46 -0600

    devolver json de nuevo comentario almacenado desde api
    Cuando se almacene un nuevo comentario en la base de datos, se devolverá su representación
    JSON. Así podrá obtenerse la fecha de creación del comentario.

--- api/noticias/comentarios/store.php
+++ api/noticias/comentarios/store.php
@@ -2,11 +2,20 @@
 
 require_once __DIR__ . '/../../utilidades.php';
 
-$usuario    = sanitizar($_POST['usuario']);
+$user       = sanitizar($_POST['usuario']);
 $email      = sanitizar($_POST['email']);
-$comentario = sanitizar($_POST['comentario']);
+$comment    = sanitizar($_POST['comentario']);
 $noticia_id = sanitizar($_POST['noticia_id']);
 $created_at = date('Y-d-m H:i:s');
 
+mysqli_begin_transaction(getMyConection(), MYSQLI_TRANS_START_READ_WRITE);
+
 query('INSERT INTO noticias_comentarios (usuario, email, comentario, noticia_id, created_at) ' .
-      "VALUES ('$usuario', '$email', '$comentario', $noticia_id, '$created_at')");
+      "VALUES ('$user', '$email', '$comment', $noticia_id, '$created_at')");
+
+$comment_id = mysqli_insert_id(getMyConection());
+$comment = mysqli_fetch_array(query("SELECT * FROM noticias_comentarios WHERE id = $comment_id"), MYSQLI_ASSOC);
+
+mysqli_commit(getMyConection());
+
+enviarRespuesta($comment);
