88c5244e89bd1f5fd2b45dd9d883ad9d3a1e1f78
Kevin Perez <kevindperezm@gmail.com>
2016-02-18 16:44:34 -0600

    reparar endpoint para almecenar comentario en api
    Esto repara errores que impedían guardar el comentario.
    
    * La función mysqli_begin_transaction() no está disponible en PHP 5.3.
    * El formato pasado a date() era 'Y-d-m H:i:s', cuando debería ser 'Y-m-d H:i:s'.

--- api/noticias/comentarios/store.php
+++ api/noticias/comentarios/store.php
@@ -6,16 +6,14 @@ $user       = sanitizar($_POST['usuario']);
 $email      = sanitizar($_POST['email']);
 $comment    = sanitizar($_POST['comentario']);
 $noticia_id = sanitizar($_POST['noticia_id']);
-$created_at = date('Y-d-m H:i:s');
+$created_at = date('Y-m-d H:i:s');
 
-mysqli_begin_transaction(getMyConection(), MYSQLI_TRANS_START_READ_WRITE);
-
+query('start transaction;');
 query('INSERT INTO noticias_comentarios (usuario, email, comentario, noticia_id, created_at) ' .
       "VALUES ('$user', '$email', '$comment', $noticia_id, '$created_at')");
 
 $comment_id = mysqli_insert_id(getMyConection());
 $comment = mysqli_fetch_array(query("SELECT * FROM noticias_comentarios WHERE id = $comment_id"), MYSQLI_ASSOC);
-
-mysqli_commit(getMyConection());
+query('commit;');
 
 enviarRespuesta($comment);
